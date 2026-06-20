be654cf5285c25f7f30f4e8cf4e2bdf211a9d3ab
kongarashiva <shivakongara91@gmail.com>
2026-06-20 06:45:42 +0000

    Updated React homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
+        <h1>Welcome to My CI/CD Pipeline Project</h1>
+ 
+        <h2>Deployed using Jenkins + Docker + GitHub</h2>
+ 
+        <p>Version 1.0</p>
+ 
       </header>
     </div>
   );
 }
-
+ 
 export default App;
+ 
